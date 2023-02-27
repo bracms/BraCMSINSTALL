29c226f0780d3782cecb7c1593faf58a63233296
New Better <1620298436@qq.com>
2023-02-27 18:19:57 +0800

    ccc

--- config/redis.php
+++ config/redis.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-	/**
-	 *   * Example DSN:
-	 *   - redis://localhost
-	 *   - redis://example.com:1234
-	 *   - redis://secret@example.com/13
-	 *   - redis:///var/run/redis.sock
-	 *   - redis://secret@/var/run/redis.sock/13
-	 */
-	'cache_dsn' => env('REDIS_URL' , 'redis://localhost/1'),
+    /**
+     *   * Example DSN:
+     *   - redis://localhost
+     *   - redis://example.com:1234
+     *   - redis://secret@example.com/13
+     *   - redis:///var/run/redis.sock
+     *   - redis://secret@/var/run/redis.sock/13
+     */
+    'cache_dsn' => env('REDIS_URL' , 'redis://localhost/1'),
+    'cache_prefix' => env('REDIS_PREFIX' , ''),
 ];
